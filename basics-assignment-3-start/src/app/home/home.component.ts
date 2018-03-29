02deb80198334440982a5ecddff867f362a16324
Alyssa Williams <alwillia@microsoft.com>
2018-03-29 11:04:12 -0700

    finished assignment 3

---
+++ basics-assignment-3-start/src/app/home/home.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+  showSecret = false;
+  log = [];
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onToggleDetails() {
+    this.showSecret = !this.showSecret;
+    // this.log.push(this.log.length + 1);
+    this.log.push(new Date());
+  }
+
+}
